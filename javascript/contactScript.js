1108cb544bf149ea63b2a195a43b87988a87a8c0
Sandijs-buls <c24456862@myTUDublin.ie>
2024-12-02 23:30:12 +0000

    .

--- javascript/contactScript.js
+++ javascript/contactScript.js
@@ -3,7 +3,7 @@ const selectElement = document.getElementById('myselect');
 // Make the select element required
 selectElement.required = true;
 
-// Optional: Add an event listener to demonstrate form validation
+ //Add an event listener to demonstrate form validation
 document.getElementsByClassName('contact-page-title').addEventListener('submit', function(event) {
     if (!selectElement.value) {
         alert('Please select an option before submitting.');
